b03fb30d5ea92f35d872c0a910a813a7fe9f4bf8
Pratham shetty <prathamshetty484@gmail.com>
2024-07-24 22:07:57 +0530

    starting webcam

--- webCam.py
+++ webCam.py
@@ -22,13 +22,16 @@ cv2.destroyAllWindows()
 """
 
 ##storing the captured video
+"""
 stream=cv2.VideoCapture(0)#0 means webcam
 if not stream.isOpened():
     print("No Cam")
     exit()
+#getting the video ka properties
 fps=stream.get(cv2.CAP_PROP_FPS)
 height=int(stream.get(4))
 width=int(stream.get(3))
+#setting up to store the stream
 output=cv2.VideoWriter("./stream.mp4",cv2.VideoWriter_fourcc('m','p','4','v'),fps=fps,frameSize=(width,height))
 
 while(True):
@@ -37,6 +40,7 @@ while(True):
         print("No more cam")
         break
     frame=cv2.resize(frame,(width,height))
+    #all frames are stored
     output.write(frame)
     cv2.imshow("Webcam",frame)
     #if q is pressed exit the while loop
@@ -45,3 +49,4 @@ while(True):
 
 stream.release()
 cv2.destroyAllWindows()
+"""
